946f19fd368129ec9a466bd0c350c9fa462c8b13
SpecialAPI <pavlov.andrei.d@gmail.com>
2024-04-02 17:35:55 +0300

    d

--- Example/ExamplePlugin.cs
+++ Example/ExamplePlugin.cs
@@ -5,6 +5,7 @@ using System;
 namespace WideCharacters.Example
 {
     [BepInPlugin("SpecialAPI.WideCharactersExample", "Wide Characters Example", "1.0.0")]
+    [BepInDependency("Bones404.BrutalAPI", BepInDependency.DependencyFlags.HardDependency)]
     public class ExamplePlugin : BaseUnityPlugin
     {
         public void Awake()
